d34e3578fd4d7443ef2a930f26d573282ff75f28
Ivan Podorozhny <ivan@podorozhny.ru>
2016-11-18 17:00:11 +0300

    bunch of changes

--- main.py
+++ main.py
@@ -1,37 +1,101 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import random
 import numpy as np
 
 
-def fft(x):
-    """A vectorized, non-recursive version of the Cooley-Tukey FFT"""
-    x = np.asarray(x, dtype=float)
-    N = x.shape[0]
+def get_window_width_from_input():
+    window_width_power = int(raw_input('Enter the window width (power of 2, between 1 and 16): '))
 
-    if np.log2(N) % 1 > 0:
-        raise ValueError("size of x must be a power of 2")
+    if window_width_power not in range(1, 16):
+        print 'Window width power of 2 must be integer between 1 and 16, "{}" given.'.format(window_width_power)
+        print 'Exiting.'
+
+        sys.exit(1)
+
+    return pow(2, window_width_power)
+
+
+def read_data(filename):
+    values = open(filename, 'r').read().split()
+
+    for i, value in enumerate(values):
+        values[i] = float(value)
+
+    return values
+
+
+def get_windows(values, width):
+    if len(values) < width:
+        print 'Window width must not be larger than values count!'
+        print 'Exiting.'
+
+        sys.exit(1)
+
+    count = 2 * (len(values) - width) / width + 1
+
+    print 'Number of windows: {}'.format(count)
+
+    results = {}
+
+    for i in xrange(0, count):
+        results[i] = {}
+
+        for j in xrange(0, width):
+            results[i][j] = values[j + i * width / 2]
+
+    return results
+
+
+def fft(values):
+    """A vectorized, non-recursive version of the Cooley-Tukey FFT"""
+    values = np.asarray(values, dtype=float)
+    values_count = values.shape[0]
+
+    if np.log2(values_count) % 1 > 0:
+        raise ValueError('values count must be a power of 2, "{}" given.'.format(values_count))
 
     # N_min here is equivalent to the stopping condition above,
     # and should be a power of 2
-    N_min = min(N, 32)
+    n_min = min(values_count, 32)
 
     # Perform an O[N^2] DFT on all length-N_min sub-problems at once
-    n = np.arange(N_min)
+    n = np.arange(n_min)
     k = n[:, None]
-    M = np.exp(-2j * np.pi * n * k / N_min)
-    X = np.dot(M, x.reshape((N_min, -1)))
+    m = np.exp(-2j * np.pi * n * k / n_min)
+    arr = np.dot(m, values.reshape((n_min, -1)))
 
     # build-up each level of the recursive calculation all at once
-    while X.shape[0] < N:
-        X_even = X[:, :X.shape[1] / 2]
-        X_odd = X[:, X.shape[1] / 2:]
-        factor = np.exp(-1j * np.pi * np.arange(X.shape[0]) / X.shape[0])[:, None]
-        X = np.vstack([X_even + factor * X_odd, X_even - factor * X_odd])
+    while arr.shape[0] < values_count:
+        arr_even = arr[:, :arr.shape[1] / 2]
+        arr_odd = arr[:, arr.shape[1] / 2:]
+        factor = np.exp(-1j * np.pi * np.arange(arr.shape[0]) / arr.shape[0])[:, None]
+        arr = np.vstack([arr_even + factor * arr_odd, arr_even - factor * arr_odd])
 
-    return X.ravel()
+    return arr.ravel()
 
-x = np.random.random(pow(2, 16))
+window_width = get_window_width_from_input()
 
-print len(np.fft.fft(x))
-print len(fft(x))
+# data = read_data('./data/CHIRP2.txt')
+# data = np.random.random(pow(2, 16))
+data = []
+for data_key in range(0, pow(2, 2)):
+    data.append(round(random.uniform(-1.0, 1.0), 8))
+
+print 'Values count: {}'.format(len(data))
+print 'Window width: {}'.format(window_width)
+
+windows = get_windows(data, window_width)
+
+result = {}
+result_2 = {}
+
+for window_key, window in windows.iteritems():
+    result[window_key] = fft(window.values())
+    # result[window_key] = np.fft.fft(window.values())
+
+print result
+
+print 'FFT done!'
